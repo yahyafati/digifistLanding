e742ec267edfe5f7b0c89643d7d9d66474b6ea1f
Yahya Fati <yfati037@gmail.com>
2022-10-04 20:31:32 +0300

    loaded fonts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-    return <div></div>;
+    return (
+        <div>
+            <p>Hello</p>
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./assets/fonts/Avenir.ttc";
+import "./assets/fonts/ITCAvantGardePro-Bk.otf";
+import "./assets/fonts/ITCAvantGardePro-Bold.otf";
+import "./assets/fonts/ITCAvantGardePro-Demi.otf";
+import "./assets/fonts/ITCAvantGardePro-Md.otf";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
