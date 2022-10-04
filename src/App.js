ae9dd0664aa92d565c9faf35783467e5c183db28
Yahya Fati <yfati037@gmail.com>
2022-10-04 20:36:10 +0300

    emotion js added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import "./App.css";
 function App() {
     return (
         <div>
-            <p>Hello</p>
+            <div>
+                <img src="./imgs/bg.png" />
+            </div>
+            <div>
+              
+            </div>
         </div>
     );
 }
