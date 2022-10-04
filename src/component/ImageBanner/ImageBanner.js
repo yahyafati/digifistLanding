8cf666f47b5dc0412c59bba7fa9bcd88921bc0e6
Yahya Fati <yfati037@gmail.com>
2022-10-04 20:47:12 +0300

    added img

--- src/component/ImageBanner/ImageBanner.js
+++ src/component/ImageBanner/ImageBanner.js
@@ -2,12 +2,24 @@ import styled from "@emotion/styled";
 import React from "react";
 
 const StyledImageBanner = styled("div")({
+    // background: `url("./imgs/bg.png")`,
+    // backgroundPosition: "center",
     background: "red",
     flex: 1,
 });
 
+const StyledImg = styled("img")({
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+});
+
 const ImageBanner = () => {
-    return <StyledImageBanner></StyledImageBanner>;
+    return (
+        <StyledImageBanner>
+            <StyledImg src="./imgs/bg.png" />
+        </StyledImageBanner>
+    );
 };
 
 export default ImageBanner;
